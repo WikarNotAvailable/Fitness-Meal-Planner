53f797790fded6d1a6365c79ccda23dd2864d2ab
WikarNotAvailable <wikar8998@gmail.com>
2022-09-14 17:00:32 +0200

    deleting code that was not being used

--- Fitness-Meal-Planner/WebAPI/Filters/PaginationFilter.cs
+++ Fitness-Meal-Planner/WebAPI/Filters/PaginationFilter.cs
@@ -5,26 +5,7 @@
     {
         private const int maxPageSize = 50;
         public int pageNumber { get; set; }
-         /*   get
-            {
-                return pageNumber;
-            }
-            set
-            {
-                pageNumber = value < 1 ? 1 : value;
-            } 
-        } */
         public int pageSize { get; set; }
-      /*  {
-            get
-            {
-                return pageSize;
-            }
-            set
-            {
-                pageSize = value > maxPageSize ? maxPageSize : value;
-            }
-        } */
         public PaginationFilter()
         {
             pageNumber = 1;
