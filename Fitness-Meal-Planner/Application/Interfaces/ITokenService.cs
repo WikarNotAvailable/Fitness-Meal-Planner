37d3d3219c84fb1bdfd622170d90ab33dac3b1ae
WikarNotAvailable <wikar8998@gmail.com>
2022-12-03 02:45:02 +0100

    finished authentication with jwt bearer

--- Fitness-Meal-Planner/Application/Interfaces/ITokenService.cs
+++ Fitness-Meal-Planner/Application/Interfaces/ITokenService.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
-    //interface used for implementation of functions connected with auth tokens
     public interface ITokenService
     {
         public string GenerateJWT(UserDto userCredentials, IConfiguration config);
